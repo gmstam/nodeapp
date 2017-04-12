562163dbd2fe31949c1e0b23ed5a6812a379eb2d
gmstam <gm.stamerra@gmail.com>
2017-04-12 14:09:35 +0200

    Added index and zipcode

--- server.js
+++ server.js
@@ -12,10 +12,10 @@ console.log('Webserver has started');
 var http = require('http');
 var fs = require('fs');
 
-http.createServer(function(req,res){
+http.createServer(function(request,response){
   fs.readFile('index.html', function(err,data){
-    res.writeHead(200,{'Content-type':'text/html'});
-    res.end(data);
+    response.writeHead(200,{'Content-type':'text/html'});
+    response.end(data);
   });
 }).listen(1337, '127.0.0.1');
 console.log('Webserver has started');
